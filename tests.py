8fbf85d874c233ae5585646724bc6f45970cdee1
steven <bobbobston252@gmail.com>
2024-05-13 15:33:55 -0500

    Wrote test3 to see if an otherwise valid password of length 6 returns False

--- tests.py
+++ tests.py
@@ -9,6 +9,9 @@ class TestFunc(unittest.TestCase):
     def test2(self):
         self.assertEqual(check_pwd("aA1!aA1"), False)
 
+    def test3(self):
+        self.assertEqual(check_pwd("aA1!aA"), False)
+
 
 if __name__ == '__main__':
     unittest.main()
